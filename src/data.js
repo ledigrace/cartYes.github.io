5ca209d84497fbd4c3793791b4260baf2c5d6071
ladygrace <cannuladygrace21@gmail.com>
2023-05-17 15:02:29 +0800

    p

---
+++ src/data.js
@@ -0,0 +1,34 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+
+const data = {
+  productData: [
+    {
+      id: 1,
+      img: img1,
+      title: 'Juicy Grapes',
+      desc: '',
+      price: 46,
+      isFavorite: false, // Add the 'isFavorite' property to track favorites
+    },
+    {
+      id: 2,
+      img: img2,
+      title: 'Red Watermelon',
+      desc: '',
+      price: 56,
+      isFavorite: false, // Add the 'isFavorite' property to track favorites
+    },
+    {
+      id: 3,
+      img: img3,
+      title: 'Juicy Orange',
+      desc: '',
+      price: 46,
+      isFavorite: false, // Add the 'isFavorite' property to track favorites
+    },
+  ],
+};
+
+export default data;
